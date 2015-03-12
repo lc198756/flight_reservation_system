a5d65deaeed0da6c81b3941964b35a2e28eb0c0a
lxynox <lxynox@users.noreply.github.com>
2015-03-12 02:49:25 -0400

    Create Airport.java
    Airport class mapped with Location class;
    communicated with AirportDom to retrieve and modify data

---
+++ src/Airport.java
@@ -0,0 +1,40 @@
+/** cs509 team: Tean 07
+ * Date: March 12. 2015
+ * Description: Class used to instantiate an airplane
+ * 				based on the information provided by the
+ * 				Database API
+ */
+ 
+public class Airport{
+          /* attributes */
+          private String name;
+          private String code;
+          private Location city;
+          
+          /* constructor of class Airport
+           * @param code short string representation for the airport 
+           * @param city represents the city where the airport locates
+           * 
+           */
+          public Airport(String code, Location city) {
+            this.name = name;
+            this.code = code;
+            this.city = city;
+          }
+          
+          /* getter and setter for Airport class
+           * used to communicate with the AirportDOM objects
+           */
+          
+          public void setName(String name) {}
+          public String getName() {}
+          
+          public void setCode(String code) {}
+          public String getCode() {}
+          
+          public void setLocation(Location city) {}
+          public Location() {}
+          
+  
+ 
+ }
